272a588afe4e2e061c26272c0cba717335425815
RichterAstell <xparis1721x@gmail.com>
2021-10-09 20:56:51 +0900

    feat: 保有ポイントの登録処理を追加

--- src/test/java/com/richter/dddsample/infrastructure/postgresql/HoldingPointRepositoryTest.java
+++ src/test/java/com/richter/dddsample/infrastructure/postgresql/HoldingPointRepositoryTest.java
@@ -34,4 +34,27 @@ class HoldingPointRepositoryTest {
         assertEquals(100, act.getHoldingPoint());
     }
 
+    @Test
+    public void 保有ポイント登録のテスト() {
+        // 検証データの作成
+        HoldingPointEntity holdingPoint = new HoldingPointEntity(1, 100);
+
+        // モックの作成
+        JdbcTemplate template = Mockito.mock(JdbcTemplate.class);
+        Mockito.when(template.queryForObject(
+                        anyString()
+                        , any(RowMapper.class)
+                        , anyInt()))
+                .thenReturn(holdingPoint);
+
+        // テスト対象の作成
+        HoldingPointRepository repository = new HoldingPointRepository(template);
+
+        // 実行
+        HoldingPointEntity act = repository.findByCustomerId(1);
+
+        // 検証
+        assertEquals(1, act.getCustomerId());
+        assertEquals(100, act.getHoldingPoint());
+    }
 }
